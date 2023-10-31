dc7faa10a33ec30aa409e0708b44395d38388c45
hdh3665 <hdh3665@naver.com>
2023-11-01 02:11:35 +0900

    #4

--- lib.js
+++ lib.js
@@ -4,13 +4,34 @@ function avg(numbers) {
 }
 
 function prime(num) {
-    //TODO
-    return false;
+    if (num <= 1) {
+        return false;
+    }
+    if (num <= 3) {
+        return true;
+    }
+    if(num%2 === 0 || num%3 === 0) {
+        return false;
+    }
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+            return false; 
+        }
+    }
+    return true;
 }
 
 function fact(num) {
-    //TODO
-    return -1;
+    if(num > 15) {
+        return -1;
+    }
+    if (num < 0) {
+        return -1;
+    }
+    if (num === 0 || num === 1) {
+        return 1;
+    }
+    return num * fact(num - 1);
 }
 
 module.exports = {
--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,31 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime(3) is true", () => {
+    expect(lib.prime(3)).toBe(true);
+});
+
+test("prime(13) is true", () => {
+    expect(lib.prime(13)).toBe(true);
+});
+
+test("prime(16) is false", () => {
+    expect(lib.prime(16)).toBe(false);
+});
+
+test("fact(3) is 6", () => {
+    expect(lib.fact(3)).toBe(6);
+});
+
+test("fact(5) is 120", () => {
+    expect(lib.fact(5)).toBe(120);
+});
+
+test("fact(6) is 720", () => {
+    expect(lib.fact(6)).toBe(720);
+});
+
+test("fact(16) is -1", () => {
+    expect(lib.fact(16)).toBe(-1);
+})
